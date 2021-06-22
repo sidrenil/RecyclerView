e46d76f20abbd0e5dacaa058298544dc58164af0
sid <dengesidrenil@gmail.com>
2021-06-22 23:57:44 +0300

    init

---
+++ app/src/main/java/com/example/rv/adapter/ItemAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.rv.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.rv.R
+import com.example.rv.model.Affirmation
+
+class ItemAdapter (
+    private val context: Context,
+    private val dataset: List<Affirmation>
+):RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+    class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(view){
+        val textview:TextView =view.findViewById(R.id.item_title)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val  item = dataset[position]
+        holder.textview.text =  context.resources.getString(item.stringResourceId)
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+}
